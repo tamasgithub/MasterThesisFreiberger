c8d1600d38553dbe63c402dce0c1c1e6b8c6807f
tamasgithub <38605940+tamasgithub@users.noreply.github.com>
2023-12-13 16:10:23 +0100

    Polishing of the main menu and I18N

---
+++ NeuralNetworkLearningApp/Assets/src/MainMenuHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject achievementsPanel;
+    public GameObject settingsPanel;
+
+    public void OpenChapters()
+    {
+        SceneManager.LoadSceneAsync("ChaptersOverview");
+    }
+
+    public void OpenAchievements()
+    {
+        mainMenuPanel.SetActive(false);
+        achievementsPanel.SetActive(true);
+    }
+
+    public void CloseAchievements()
+    {
+        mainMenuPanel.SetActive(true);
+        achievementsPanel.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
