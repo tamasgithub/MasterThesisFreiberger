6982a3a08834e8735bf5326f06d264b936937441
tamasgithub <38605940+tamasgithub@users.noreply.github.com>
2024-06-02 21:38:44 +0200

    Changed materials for newer unity editor version. Moved src files into src folder. Added the required index.html file for hosting the app for the browser.

--- NeuralNetworkLearningApp/Assets/src/World/TalkInteraction.cs
+++ NeuralNetworkLearningApp/Assets/src/World/TalkInteraction.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Networking.Types;
 using UnityEngine.UI;
 
 public class TalkInteraction : Interaction
